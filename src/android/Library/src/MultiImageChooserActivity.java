1e15ff06eb4c3e76595705d81045ec009f48581d
Paul Kocher <pkocher@axioconcept.com>
2020-10-28 13:59:41 +0100

    fix imports

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -37,7 +37,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -47,6 +47,7 @@ import com.synconset.FakeR;
 import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.app.ProgressDialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
@@ -57,13 +58,16 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.ImageDecoder;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.SparseBooleanArray;
 import android.view.Display;
@@ -100,7 +104,7 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
     private static final int CURSORLOADER_THUMBS = 0;
     private static final int CURSORLOADER_REAL = 1;
 
-    private Map<String, Integer> fileNames = new HashMap<String, Integer>();
+    private Map<String, Integer> fileNames = new LinkedHashMap<String, Integer>();
 
     private SparseBooleanArray checkStatus = new SparseBooleanArray();
 
@@ -426,7 +430,7 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
     /*********************
     * Nested Classes
     ********************/
-    private class SquareImageView extends ImageView {
+    private class SquareImageView extends androidx.appcompat.widget.AppCompatImageView {
         public SquareImageView(Context context) {
 			super(context);
 		}
@@ -522,6 +526,11 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
                     BitmapFactory.Options options = new BitmapFactory.Options();
                     options.inSampleSize = 1;
                     options.inJustDecodeBounds = true;
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                        _tryToGetBitmap(file, options);
+                    } else {
+                        BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                    }
                     BitmapFactory.decodeFile(file.getAbsolutePath(), options);
                     int width = options.outWidth;
                     int height = options.outHeight;
@@ -620,15 +629,64 @@ public class MultiImageChooserActivity extends AppCompatActivity implements
             finish();
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.P)
+        private Bitmap _tryToGetBitmap(File file, BitmapFactory.Options options) throws IOException, OutOfMemoryError {
+            Bitmap bmp = null;
+            String[] projection = new String[] { MediaStore.Images.ImageColumns._ID,
+                    MediaStore.Images.ImageColumns.DISPLAY_NAME, MediaStore.Images.ImageColumns.WIDTH,
+                    MediaStore.Images.ImageColumns.HEIGHT };
+            String selection = MediaStore.Images.ImageColumns.DISPLAY_NAME + " = ?";
+            String[] selectionArguments = { file.getName() };
+            Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+                    selection, selectionArguments, null);
+            if (cursor != null && cursor.getCount() > 0) {
+                cursor.moveToFirst();
+
+                if (options != null && options.inJustDecodeBounds) {
+                    options.outWidth = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns.WIDTH));
+                    options.outHeight = cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns.HEIGHT));
+                    cursor.close();
+
+                    return null;
+                }
+
+                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        cursor.getInt(cursor.getColumnIndex(MediaStore.Images.ImageColumns._ID)));
+                cursor.close();
+
+                if (options != null) {
+                    bmp = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), imageUri),
+                            new ImageDecoder.OnHeaderDecodedListener() {
+                                public void onHeaderDecoded(ImageDecoder decoder, ImageDecoder.ImageInfo info,
+                                        ImageDecoder.Source source) {
+                                    decoder.setTargetSampleSize(options.inSampleSize);
+                                }
+                            });
+                } else {
+                    bmp = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), imageUri));
+                }
+            }
+
+            if (bmp == null) {
+                throw new IOException("The image file could not be opened.");
+            }
+
+            return bmp;
+        }
+
         private Bitmap tryToGetBitmap(File file,
                                       BitmapFactory.Options options,
                                       int rotate,
                                       boolean shouldScale) throws IOException, OutOfMemoryError {
             Bitmap bmp;
-            if (options == null) {
-                bmp = BitmapFactory.decodeFile(file.getAbsolutePath());
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                bmp = _tryToGetBitmap(file, options);
             } else {
-                bmp = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                if (options == null) {
+                    bmp = BitmapFactory.decodeFile(file.getAbsolutePath());
+                } else {
+                    bmp = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                }
             }
 
             if (bmp == null) {
